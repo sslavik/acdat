9c386b5dcb2e3a0c8634ee465befccefb3d73656
sslavik <sslavikshy@gmail.com>
2019-11-13 12:47:23 +0100

    Actividad 35 Modificada y Enviada y Actividad Suplementaria

--- Actividad35/src/actividad35/Actividad36.java
+++ Actividad35/src/actividad35/Actividad36.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package actividad35;
+package actividad36;
 
 import java.sql.*;
 import java.util.HashMap;
