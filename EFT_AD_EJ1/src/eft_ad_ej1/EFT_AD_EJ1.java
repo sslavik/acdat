2c4e9cf4b185b8b863653168c42e6157c45688b5
sslavik <sslavikshy@gmail.com>
2019-12-02 12:47:53 +0100

    EJ final 1 añadido

--- EFT_AD_EJ1/src/eft_ad_ej1/EFT_AD_EJ1.java
+++ EFT_AD_EJ1/src/eft_ad_ej1/EFT_AD_EJ1.java
@@ -5,6 +5,11 @@
  */
 package eft_ad_ej1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.sql.*;
+
 /**
  *Haz un programa que permita navegar de forma interactiva por los contenidos de la tabla CLIENTES creada con un programa de ejemplo anterior.
  * Primero el programa debe realizar una consulta para obtener los contenidos de la tabla, y debe mostrar el mensaje “fila 1” y el contenido de la fila,
@@ -28,8 +33,93 @@ public class EFT_AD_EJ1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException, IOException {
         // TODO code application logic here
+        
+        String host = "remotemysql.com";
+        String port = "3306";
+        String database = "np5tn1dien";
+        String addParams = "useSSL=false";
+        String user = "np5tn1dien";
+        String passwd = "v39eXDt1Z6";
+        
+        String url = String.format("jdbc:mysql://%s:%s/%s?%s", host,port,database,addParams);
+        
+        Connection c = DriverManager.getConnection(url,user,passwd);
+        
+        
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))){
+            
+            System.out.println("OPCIONES");
+            System.out.println("'.' SALIR");
+            System.out.println("'k' SIGUIENTE LINEA");
+            System.out.println("'d' ANTERIOR LINEA");
+            System.out.println("'0-9' SALTO A NUM LINEA");
+            
+            String opcion = ""; 
+            int posicion = 0;
+            int limite = 0;
+            System.out.println("MYSQL >>> Introduce una opción");
+
+            do{
+                try{
+                    opcion = br.readLine();
+                    
+                    int i = 0;
+                    PreparedStatement ps0 = c.prepareStatement("select count(*) as total from CLIENTES");
+                    PreparedStatement ps = c.prepareStatement("select * from CLIENTES limit ?,?;");
+                    
+                    ResultSet rsCount = ps0.executeQuery();
+                    
+                    rsCount.next();
+                    
+                    limite = rsCount.getInt("total");
+                    
+                    
+                    switch(opcion){
+                    case "k" : 
+                        if(posicion < limite){
+                            ps.setInt(++i, posicion++);
+                            ps.setInt(++i, posicion);
+                        }
+                        else {
+                            System.out.println("No existen más registros");
+                        }
+                        break;
+                    case "d" : 
+                        if(posicion > 1){
+                            ps.setInt(++i, posicion -= 2); 
+                            ps.setInt(++i, ++posicion); 
+                        } else {
+                            System.out.println("No existen más registros");
+                        }
+                        break;
+                    case "." : 
+                        break;
+                    default :
+                        int salto = Integer.parseInt(opcion);
+                        break;
+                    }
+                    ResultSet rs = ps.executeQuery();
+                        
+                        rs.next();
+                        
+                        String dni = rs.getString("DNI");                        
+                        String ape = rs.getString("APELLIDOS");
+                        String cp = rs.getString("CP");
+                        
+                        System.out.println("Fila " + posicion + " | DNI : " + dni + " APELLIDOS : "+ ape + " CP : " + cp );
+                }
+                catch (Exception e) {
+                    System.err.println(e.getMessage());
+                }
+                System.out.println("MYSQL >>> Introduce una opción");
+
+                
+            } while(opcion.charAt(0) != '.');
+                
+        }
+        
     }
     
 }
