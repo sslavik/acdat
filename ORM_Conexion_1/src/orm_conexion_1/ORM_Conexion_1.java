81d84b5ca20eff4db5425cf66398813a3bef8baf
sslavik <sslavikshy@gmail.com>
2019-12-11 12:46:35 +0100

    Añadido ORM Hibernate

---
+++ ORM_Conexion_1/src/orm_conexion_1/ORM_Conexion_1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package orm_conexion_1;
+
+import net.sf.ehcache.hibernate.HibernateUtil;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+/**
+ *
+ * @author usuario
+ */
+public class ORM_Conexion_1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
