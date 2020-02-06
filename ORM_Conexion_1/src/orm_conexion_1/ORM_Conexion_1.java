4b582e377a31d5a8cf6f9d7ea31a54a58bb57c28
sslavik <sslavikshy@gmail.com>
2020-02-06 14:41:37 +0100

    Add ORM Jerarquias NOT WORKING

--- ORM_Conexion_1/src/orm_conexion_1/ORM_Conexion_1.java
+++ ORM_Conexion_1/src/orm_conexion_1/ORM_Conexion_1.java
@@ -5,7 +5,6 @@
  */
 package orm_conexion_1;
 
-import net.sf.ehcache.hibernate.HibernateUtil;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 import ORM.*;
@@ -14,35 +13,13 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 import org.hibernate.hql.internal.ast.util.SessionFactoryHelper;
-import org.hibernate.jpa.boot.internal.EntityManagerFactoryBuilderImpl.MetadataSources;
 import org.hibernate.mapping.MetadataSource;
-import org.hibernate.metamodel.Metadata;
 
 /**
  *
  * @author Vyacheslav Shylyayev
  */
 
-class HibernateUtil {
-
-    private static final SessionFactory sessionFactory;
-    
-    static {
-        try {
-            // Create the SessionFactory from standard (hibernate.cfg.xml) 
-            // config file.
-            sessionFactory = new Configuration().configure().buildSessionFactory();
-        } catch (Throwable ex) {
-            // Log the exception. 
-            System.err.println("Initial SessionFactory creation failed." + ex);
-            throw new ExceptionInInitializerError(ex);
-        }
-    }
-    
-    public static SessionFactory getSessionFactory() {
-        return sessionFactory;
-    }
-}
 
 public class ORM_Conexion_1 {
 
@@ -55,48 +32,51 @@ public class ORM_Conexion_1 {
         
         // VAMOS A CREAR OBJETOS DE LAS TABLAS
         // SEDE
-        Sede sede_1 = new Sede(0,"sede_1");
+        Sede sede_1 = new Sede("sede_1");
         // DEPARTAMENTOS
-        Departamento dpto_1 = new Departamento(0, sede_1, "Recursos Humanos");
-        Departamento dpto_2 = new Departamento(0, sede_1, "Marketing");
+        Departamento dpto_1 = new Departamento(sede_1, "Recursos Humanos");
+        Departamento dpto_2 = new Departamento(sede_1, "Marketing");
         // EMPLEADOS
         Empleado e1 = new Empleado("X214512A", dpto_1, "Pablo Torres");
         Empleado e2 = new Empleado("12412523D", dpto_1, "Alfonso Gil");
         Empleado e3 = new Empleado("Y1950929S", dpto_2, "Martin Martinez");
         Empleado e4 = new Empleado("X546554D", dpto_2, "Arthur Shelby");
         
+        Session s = HibernateUtil.getSessionFactory().openSession();
+        
         // Creamos un Servicio De Registro Seguro
-        try ( Session s = HibernateUtil.getSessionFactory().openSession()) {
-            Transaction t = null;
+        Transaction t = null;
 
-            try {
-                t = s.beginTransaction();
+        try {
+            t = s.beginTransaction();
 
-                ORM.Sede sede = new ORM.Sede();
-                sede.setNomSede("MÁLAGA");
-                s.save(sede);
+            ORM.Sede sede = new ORM.Sede();
+            sede.setNomSede("MÁLAGA");
+            s.saveOrUpdate(sede);
 
-                ORM.Departamento depto = new ORM.Departamento();
-                depto.setNomDepto("INVESTIGACIÓN Y DESARROLLO");
-                depto.setSede(sede);
-                s.save(depto);
+            ORM.Departamento depto = new ORM.Departamento();
+            depto.setNomDepto("INVESTIGACIÓN Y DESARROLLO");
+            depto.setSede(sede);
+            s.saveOrUpdate(depto);
 
-                ORM.Empleado emp = new ORM.Empleado();
-                emp.setDni("56789012B");
-                emp.setNomEmp("SAMPER");
-                emp.setDepartamento(depto);
-                s.save(emp);
+            ORM.Empleado emp = new ORM.Empleado();
+            emp.setDni("56789012B");
+            emp.setNomEmp("SAMPER");
+            emp.setDepartamento(depto);
+            
+            s.saveOrUpdate(emp);
 
-                t.commit();
+            t.commit();
 
-            } catch (Exception e) {
-                e.printStackTrace(System.err);
-                if (t != null) {
-                    t.rollback();
-                }
+        } catch (Exception e) {
+            e.printStackTrace(System.err);
+            if (t != null) {
+                t.rollback();
             }
-
         }
+        s.close();
+
+
         
         
       
